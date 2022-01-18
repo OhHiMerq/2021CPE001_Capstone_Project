3d6e9e8d8c28bce341366b95505a60cd11083818
OhHiMerq <dimailigv@gmail.com>
2022-01-18 14:13:11 +0800

    Filtering Recipes

---
+++ Firebase Database Setup/FilteringRecipes.py
@@ -0,0 +1,7 @@
+choosedRecipes = ['Avocado Sago Jelly',
+    'Bibingka',
+    'Champorado',
+    'Ginisang Sayote',
+    'Ginataang Puso ng Saging',]
+
+print("Number of Recipes: "+str(len(choosedRecipes)))
